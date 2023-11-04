734f094f0bad75865c8e2791924540fcf0a1cb60
cr <cr@my-little-marefriend.local>
2023-11-04 16:50:55 -0700

    Add a place for global constants and vars

---
+++ godot/GLOBALS.gd
@@ -0,0 +1,45 @@
+#         _                        ___         ___        _       
+#        | |__  _   _ _ __  _ __  / _ \  __ _ / _ \  __ _/ | __ _ 
+#        | '_ \| | | | '_ \| '_ \| | | |/ _` | | | |/ _` | |/ _` |
+#        | | | | |_| | |_) | | | | |_| | (_| | |_| | (_| | | (_| |
+#        |_| |_|\__, | .__/|_| |_|\___/ \__, |\___/ \__, |_|\__,_|
+#               |___/|_|                |___/       |___/         
+#                         ...made for https://itch.io/jam/lsdjam-2023
+# 
+#------------------------------------------------------------------------------    
+# MIT License
+# 
+# Copyright (c) 2023 illegalinstruction
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+
+extends Node
+
+#--- CONSTANTS -----------------------------------------------------------------
+const FPS : float = 48.0;
+const GRAVITY : float = 9.8 / FPS;
+const COEFF_OF_FRICTION : float =  0.9975;
+
+#--- VARS ----------------------------------------------------------------------
+
+#--- FUNCTIONS -----------------------------------------------------------------
+
+func _ready():
+    return;
