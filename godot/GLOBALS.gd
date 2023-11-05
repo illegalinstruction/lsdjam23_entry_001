3260d53fbe903a73797eed65847ecc1ed69baab8
cr <cr@my-little-marefriend.local>
2023-11-04 18:13:58 -0700

    BGM, sound volume and other options management, partial work on #10, but more work is needed.

--- godot/GLOBALS.gd
+++ godot/GLOBALS.gd
@@ -33,13 +33,85 @@
 extends Node
 
 #--- CONSTANTS -----------------------------------------------------------------
+const SCREENWIPE_MAX_TICKS : int = 60;
 const FPS : float = 48.0;
 const GRAVITY : float = 9.8 / FPS;
 const COEFF_OF_FRICTION : float =  0.9975;
+const MAX_VOLUME : int = 8;
+const BGM_FADE_PER_TICK = 0.933;
+
+const DATA_BASE     : String = "user://below_data";
+const OPTIONS_PATH  : String = DATA_BASE + "-options";
+const SAVEGAME_PATH : String = DATA_BASE + "-savegame";
 
 #--- VARS ----------------------------------------------------------------------
+# configuration
+var sfx_vol             : int   = 3;
+var music_vol           : int   = 3;
+var use_joystick        : bool  = true;
+
+# BGM management
+var curr_bgm            : int   = 0;
+var next_bgm            : int   = 0;
+var music_fadeout_clock : int   = 0;
+
+# other 
+var ui_font             : Font  = null;
 
 #--- FUNCTIONS -----------------------------------------------------------------
 
+func get_music_vol_in_db():
+    if (music_fadeout_clock < SCREENWIPE_MAX_TICKS):
+        return linear2db((GLOBALS.music_vol / float(MAX_VOLUME)) * pow(BGM_FADE_PER_TICK,music_fadeout_clock));
+    if (curr_bgm != next_bgm):
+        return linear2db(0);
+    return linear2db(GLOBALS.music_vol / float(MAX_VOLUME));
+
+#-------------------------------------------------------------------------------
+
+func get_sfx_vol_in_db():
+    return linear2db(GLOBALS.sfx_vol / float(MAX_VOLUME));
+
+#-------------------------------------------------------------------------------
+
+func save_options_data():
+    var fout : File = File.new();
+
+    var error = fout.open(OPTIONS_PATH, fout.WRITE);
+    if (error > 0):
+        print_debug("could not open options file for writing! error code: " + str(error));
+        return; # the show must go on...?
+        
+    sfx_vol = int(clamp(sfx_vol,0, MAX_VOLUME));
+    fout.store_8(sfx_vol);
+    music_vol = int(clamp(music_vol, 0, MAX_VOLUME));
+    fout.store_8(music_vol);
+    fout.store_8(use_joystick);
+    fout.close();
+    
+    return;
+    
+#-------------------------------------------------------------------------------
+
+func load_options_data():
+    var fin : File = File.new();
+        
+    if (fin.file_exists(OPTIONS_PATH)):
+        var error = fin.open(OPTIONS_PATH, fin.READ);
+        if (error > 0):
+            print_debug("could not open options file for reading! error code: " + str(error));
+            return; 
+            
+        sfx_vol = fin.get_8();
+        music_vol = fin.get_8();
+        use_joystick = fin.get_8();
+        fin.close();
+    else:
+        save_options_data();
+    return;
+
+#-------------------------------------------------------------------------------
+
 func _ready():
     return;
+    
